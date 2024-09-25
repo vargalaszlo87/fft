bae0299aded44e817de27e2a723a9d7a2ad1cc09
Laszlo Varga <varga.laszlo.drz@gmail.com>
2024-09-25 21:21:26 +0200

    add new tests

--- src/fft.c
+++ src/fft.c
@@ -1,12 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <complex.h>
-#include <stddef.h>
-
 #ifndef M_PI
 #define M_PI 3.1415926538
 #endif
+
+typedef struct fft_ {
+    double samplingPeriod;
+    int samplingSize;
+} fftSetup;
+
+enum {
+    HAMMING,
+    HANNING,
+    BLACKMAN,
+    BARTLETT,
+    enumSize
+};
+
+// windows
+void Hamming(complex double* signal, int size) {
+	int i = -1;
+    while (++i < size)
+        signal[i] *= 0.54 - 0.46 * cos(2.0 * M_PI * i / (size - 1));
+}
+
+void Hanning(complex double* signal, int size) {
+    int i = -1;
+    while (++i < size)
+        signal[i] *= 0.5 * (1.0 - cos(2.0 * M_PI * i / (size - 1)));
+}
+
+void Blackman(complex double* signal, int size) {
+    int i = -1;
+    while (++i < size)
+        signal[i] *= 0.42 - 0.5 * cos(2.0 * M_PI * i / (size - 1)) + 0.08 * cos(4.0 * M_PI * i / (size - 1));
+}
+
+void Bartlett(complex double* signal, int size) {
+    int i = -1;
+    while (++i < size)
+        signal[i] *= 1.0 - fabs((i - (size - 1) / 2.0) / ((size - 1) / 2.0));
+}
 
 void fft(complex double* x, int size) {
     if (size <= 1)
@@ -31,4 +63,17 @@ void fft(complex double* x, int size) {
 
     free(even);
     free(odd);
+}
+
+double* results(complex double* signal, double samplingPeriod, int size, int type) {
+    double* out = (double*)calloc(size / 2, sizeof(double));
+    if (!out) {
+        fprintf(stderr, "Failed memory allocation.\n");
+        exit(1);
+    }
+    double samplingFrequency = 1 / samplingPeriod;
+    for (int k = 0; k < SAMPLE / 2; k++) {
+        *(out + k) = (type) ? sqrt(creal(signal[k]) * creal(signal[k]) + cimag(signal[k]) * cimag(signal[k])) : (double)k * samplingFrequency / size;
+    }
+    return out;
 }
--- tests/test_fft.c
+++ tests/test_fft.c
@@ -1,9 +1,54 @@
-// tests/test_fft.c
 #include <stdio.h>
-#include <complex.h>
+#include <stdlib.h>
 #include <math.h>
+#include <complex.h>
 #include <assert.h>
-#include "../src/fft.c"
+
+#include "../src/fft.c"  
+
+#define EPSILON 1e-6
+
+void test_hamming_window() {
+    int size = 4;
+    complex double signal[] = {1.0, 2.0, 3.0, 4.0};
+    complex double expected[] = {0.54, 1.08, 1.62, 2.16}; 
+
+    Hamming(signal, size);
+
+    for (int i = 0; i < size; i++) {
+        assert(fabs(creal(signal[i]) - creal(expected[i])) < EPSILON);
+    }
+
+    printf("Hamming window test passed.\n");
+}
+
+void test_hanning_window() {
+    int size = 4;
+    complex double signal[] = {1.0, 2.0, 3.0, 4.0};
+    complex double expected[] = {0.0, 1.5, 3.0, 0.0}; 
+
+    Hanning(signal, size);
+
+    for (int i = 0; i < size; i++) {
+        assert(fabs(creal(signal[i]) - creal(expected[i])) < EPSILON);
+    }
+
+    printf("Hanning window test passed.\n");
+}
+
+void test_blackman_window() {
+    int size = 4;
+    complex double signal[] = {1.0, 2.0, 3.0, 4.0};
+    complex double expected[] = {0.42, 1.26, 1.62, 2.16}; 
+
+    Blackman(signal, size);
+
+    for (int i = 0; i < size; i++) {
+        assert(fabs(creal(signal[i]) - creal(expected[i])) < EPSILON);
+    }
+
+    printf("Blackman window test passed.\n");
+}
 
 int compare_complex(complex double a, complex double b, double tolerance) {
     return (cabs(a - b) < tolerance);
@@ -23,8 +68,14 @@ void test_fft() {
     }
 }
 
+
 int main() {
+    test_hamming_window();
+    test_hanning_window();
+    test_blackman_window();
     test_fft();
-    printf("All tests passed!\n");
+
+    printf("All tests passed.\n");
     return 0;
 }
+
